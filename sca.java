c929d522a2003664e05b4958548883a3318f735d
Pagadala Bhavana <pagadalabhavana7586@gmail.com>
2026-07-04 20:51:03 +0530

    Create ScientificCalculatorApplication class

---
+++ sca.java
@@ -0,0 +1,12 @@
+package com.example.calculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScientificCalculatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScientificCalculatorApplication.class, args);
+    }
+}
